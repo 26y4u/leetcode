fe7b075e44673a5ce85ceacd97567169cb840f9c
songlijun <songlijun@inspur.com>
2020-07-02 17:06:26 +0800

    栈

--- solution/src/stack/application.java
+++ solution/src/stack/application.java
@@ -1,7 +1,6 @@
 package stack;
 
 import java.io.IOException;
-
 public class application {
     public static void main(String[] args) throws IOException {
         System.out.println("hello world!");
@@ -10,8 +9,14 @@ public class application {
 //        cq.outPut();
         solution sq = new solution();
         String[] arr = {"4","13","5","/","+"};
-        sq.evalRPN(arr);
-
+        int[] xx = {1,1,1,1,1};
+        sq.findTargetSumWays1(xx,3);
+        solution.TreeNode tr1 = new solution.TreeNode(1);
+        solution.TreeNode tr2 = new solution.TreeNode(2);
+        solution.TreeNode tr3 = new solution.TreeNode(3);
+        tr1.right = tr2;
+        tr2.left = tr3;
+        sq.inorderTraversal1(tr1);
 
     }
 }
--- solution/src/stack/solution.java
+++ solution/src/stack/solution.java
@@ -2,6 +2,7 @@ package stack;
 
 import org.w3c.dom.Node;
 
+import javax.swing.*;
 import java.util.*;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -131,6 +132,152 @@ public class solution {
             }
         }
         return stack.isEmpty()?0:stack.pop();
+    }
+
+    /**
+     * 133. 克隆图(深拷贝)
+     * clone-graph
+     *
+     * @author: 1004♥
+     * @CreateDate 2020-06-29
+     * @UpdateDate 2020-06-29
+     */
+    class Node {
+        public int val;
+        public List<Node> neighbors;
+
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+    Map<Node, Node> map = new HashMap<>();
+    public Node cloneGraph(Node node) {
+        if(node == null) return null;
+        Node copy = new Node(node.val, new ArrayList<>());
+        map.put(node, copy);
+        for(Node neighbor : node.neighbors){
+            if(map.containsKey(neighbor)){
+                copy.neighbors.add(map.get(neighbor));
+            }else{
+                copy.neighbors.add(cloneGraph(neighbor));
+            }
+        }
+        return copy;
+    }
+
+    /**
+     * 494. 目标和(枚举)
+     * target-sum
+     *
+     * @author: 1004♥
+     * @CreateDate 2020-06-30
+     * @UpdateDate 2020-06-30
+     */
+    int count = 0;
+    public void dfs(int i,int sum,int[] nums,int S){
+        if(sum == S && i == nums.length){
+            count++;
+        }
+        else {
+            dfs(i+1,sum+nums[i],nums,S);
+            dfs(i+1,sum-nums[i],nums,S);
+        }
+    }
+    public int findTargetSumWays(int[] nums, int S) {
+        int sum = 0,i = 0;
+        dfs(i,sum,nums,S);
+        return count;
+    }
+
+    /**
+     * 494. 目标和(动态规划)
+     * target-sum
+     *
+     * SUM(正值) + (-)SUM(负值) = target
+     * SUM(正值) + SUM(负值) = SUM
+     * 2*SUM(正值)-SUM = target
+     * 问题转化为-> 求几个值加起来=(SUM+TARGET)/2时的解法
+     * ∴ SUM+target为奇数时，解法为0
+     *
+     * dp[i]: 目标和为i的解法数量  i=(sum+target)/2 dp[0]: 1;目标和为0只有一种解法
+     * dp[i] = dp[i-nums[0]]+...+dp[i-nums[j]]; j=0,1,2,...,nums.length-1;
+     *
+     * @author: 1004♥
+     * @CreateDate 2020-06-30
+     * @UpdateDate 2020-06-30
+     */
+
+    public int findTargetSumWays1(int[] nums, int S) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum < S || (sum + S) % 2 > 0) {
+            return 0;
+        }
+        int s = (sum + S)/2; //转化为求dp[s]
+        int dp[] = new int[s+1];
+        dp[0] = 1;
+        for(int i = 0;i<nums.length;i++){  // 保证每个数据只添加或删除一次，外层index遍历
+            for (int j = s;j >= nums[i];j--)
+                dp[j] += dp[j - nums[i]];
+        }
+        return dp[s];
+    }
+    /**
+     * 94. 二叉树的中序遍历
+     * binary-tree-inorder-traversal
+     *
+     * @author: 1004♥
+     * @CreateDate 2020-07-02
+     * @UpdateDate 2020-07-02
+     */
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    List<Integer> list = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {// 递归
+        if (root == null){
+            return list;
+        }
+        if(root.left != null){
+            inorderTraversal(root.left);
+        }
+        list.add(root.val);
+        if (root.right!=null){
+            inorderTraversal(root.right);
+        }
+        return list;
+    }
+    public List<Integer> inorderTraversal1(TreeNode root) {// 用栈
+        List<Integer> res = new ArrayList<>();
+        TreeNode curr = root;
+        Stack<TreeNode> stack = new Stack<>();
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            res.add(curr.val);
+            curr = curr.right;
+        }
+        return res;
     }
 }
 
